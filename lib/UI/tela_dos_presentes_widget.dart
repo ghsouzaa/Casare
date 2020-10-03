75e4bd4c431adcd95aace0407e61bb3e9f14a56a
Eduardo Yamauchi <yamauchieduardo@gmail.com>
2020-10-03 00:13:29 -0300

    ref: Tela dos presentes widget

--- lib/UI/tela_dos_presentes_widget.dart
+++ lib/UI/tela_dos_presentes_widget.dart
@@ -5,29 +5,23 @@ class TelaDosPresentesWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Padding(
         padding: EdgeInsets.fromLTRB(30, 0, 30, 75),
-        child: (Card(
+        child: Card(
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              const ListTile(
-                title: Text('Visitar Torre Eiffel'),
-                subtitle: Text('RS 150,00'),
-              ),
+              const ListTile(title: Text('Visitar Torre Eiffel'), subtitle: Text('RS 150,00')),
               Row(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: <Widget>[
                   TextButton(
                     child: const Text('PRESENTEAR'),
-                    onPressed: () {
-                      /* ... */
-                    },
+                    onPressed: () => print('PRESENTER'),
                   ),
-                  const SizedBox(width: 8),
-                  const SizedBox(width: 8),
+                  const SizedBox(width: 16),
                 ],
               ),
             ],
           ),
-        )),
+        ),
       );
 }
